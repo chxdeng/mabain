a9d2aa52d5f41fef1ce2f84d2934fe104cf63741
chadeng <chadeng@cisco.com>
2017-07-25 15:42:50 +0000

    adding consts class

---
+++ src/mabain_consts.cpp
@@ -0,0 +1,12 @@
+#include "mabain_consts.h"
+
+namespace mabain {
+
+const int CONSTS::ACCESS_MODE_READER     = 0x0;
+const int CONSTS::ACCESS_MODE_WRITER     = 0x1;
+const int CONSTS::OPTION_ALL_PREFIX      = 0x1;
+const int CONSTS::OPTION_FIND_AND_DELETE = 0x2;
+const int CONSTS::MAX_KEY_LENGHTH        = 256;
+const int CONSTS::MAX_DATA_SIZE          = 0x7FFF;
+
+}
