5828667f287ae94917462f945d28fa40d9f206bb
Satyajit Padalkar <spadalkar@gmail.com>
2018-03-15 16:31:57 -0400

    Update resource collection example

--- examples/mb_rc_test.cpp
+++ examples/mb_rc_test.cpp
@@ -16,42 +16,228 @@
 
 // @author Changxue Deng <chadeng@cisco.com>
 
+#include <assert.h>
 #include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <atomic>
+
 #include <mabain/db.h>
+
+#include "test_key.h"
 
 using namespace mabain;
 
-const char *db_dir = "/var/tmp/db_test/mabain/";
+#define DB_SIZE 128ULL*1024*1024
 
-// Resource collection test
-int main(int argc, char *argv[])
+static int max_key = 10000000;
+static std::atomic<int> write_index;
+static bool stop_processing = false;
+static std::string mbdir = "/var/tmp/mabain_test/";
+
+std::string key_list[] = {
+    "facebook",
+    "facetime",
+    "faceoff",
+    "furious",
+    "falcons",
+    "fences",
+    "fireman",
+    "fantasticfour",
+    "fragile",
+    "frankenstein"
+};
+
+static void* insert_thread(void *arg)
 {
-    if(argc == 2) {
-        db_dir = argv[1];
+    int curr_key;
+    TestKey mkey(MABAIN_TEST_KEY_TYPE_SHA_256);
+    std::string kv;
+    DB *db_r = new DB(mbdir.c_str(), CONSTS::ReaderOptions(), 128LL*1024*1024, 128LL*1024*1024);
+    // If a reader wants to perform DB update, the async writer pointer must be set.
+    assert(db_r->is_open());
+    assert(db_r->SetAsyncWriterPtr((DB *) arg) == MBError::SUCCESS);
+    assert(db_r->AsyncWriterEnabled());
+
+    while(!stop_processing) {
+        curr_key = write_index.fetch_add(1, std::memory_order_release);
+        kv = mkey.get_key(curr_key);
+        if(curr_key < max_key) {
+            assert(db_r->Add(kv, kv) == MBError::SUCCESS);
+        } else {
+            stop_processing = true;
+            break;
+        }
     }
-    std::string cmd = std::string("mkdir -p ") + db_dir;
+
+    // Reader must unregister the async writer pointer
+    assert(db_r->UnsetAsyncWriterPtr((DB *) arg) == MBError::SUCCESS);
+    db_r->Close();
+    delete db_r;
+    return NULL;
+}
+
+static void SetTestStatus(bool success)
+{
+    std::string cmd;
+    if(success) {
+        cmd = std::string("touch ") + mbdir + "/_success";
+    } else {
+        cmd = std::string("rm ") + mbdir + "/_success >" + mbdir + "/out 2>" + mbdir + "/err";
+    }
     if(system(cmd.c_str()) != 0) {
     }
-    mabain::DB::SetLogFile("/var/tmp/mabain_test/mabain.log");
-    mabain::MBConfig mbconf;
-    memset(&mbconf, 0, sizeof(mbconf));
-    mbconf.mbdir = db_dir;
-    mbconf.options = CONSTS::WriterOptions();
-    mbconf.block_size_index = 1024ULL*1024*1024;
-    mbconf.block_size_data = 1024ULL*1024*1024;
-    mbconf.num_entry_per_bucket = 10000;
-    mbconf.max_num_index_block = 1000000000;
-    mbconf.max_num_data_block = 1000000000;
-    mbconf.memcap_index = (unsigned long long)128*1024*1024;
-    mbconf.memcap_data = (unsigned long long)0;
-    DB db(mbconf);
-    if(!db.is_open()) {
-        std::cerr << "failed to open mabain db: " << db.StatusStr() << "\n";
-        exit(1);
+}
+
+static void Lookup()
+{
+    TestKey mkey(MABAIN_TEST_KEY_TYPE_SHA_256);
+    std::string kv;
+    DB *db_r = new DB(mbdir.c_str(), CONSTS::ReaderOptions(), 128LL*1024*1024, 128LL*1024*1024);
+    assert(db_r->is_open());
+    MBData mbd;
+
+    for(int i = 0; i < 100; i++) {
+        kv = mkey.get_key(i);
+        assert(db_r->Find(kv, mbd) == MBError::SUCCESS);
+        assert(kv == std::string((const char *)mbd.buff, mbd.data_len));
+    }
+    db_r->Close();
+    delete db_r;
+}
+
+static void GarbageLookup()
+{
+    std::cout << "\nCalling Lookups during Garbage Collection" << std::endl;
+    std::string kv;
+    DB *db_r = new DB(mbdir.c_str(), CONSTS::ReaderOptions(), 128LL*1024*1024, 128LL*1024*1024);
+    assert(db_r->is_open());
+    MBData mbd;
+
+    for(int i = 0; i < 10; i++) {
+        int rval = db_r->Find(key_list[i], mbd);
+        if(rval != MBError::SUCCESS) {
+            std::cout << key_list[i] << ": " << MBError::get_error_str(rval) << std::endl;
+        } else {
+            std::cout << key_list[i] << ": " << std::string((const char *)mbd.buff, mbd.data_len) << std::endl;
+            assert(rval == MBError::SUCCESS);
+            assert(key_list[i] == std::string((const char *)mbd.buff, mbd.data_len));
+        }
     }
 
-    db.CollectResource(1, 0xFFFFFFFFFFFF, 0xFFFFFFFFFFFF, 0xFFFFFFFFFFFF);
-    db.Close();
+    db_r->Close();
+    delete db_r;
+}
+
+static void Deletekeys()
+{
+    std::cout << "\nStart Deletekeys ()" << std::endl;
+    std::string kv;
+
+    int options = CONSTS::WriterOptions() | CONSTS::ReaderOptions() | CONSTS::ASYNC_WRITER_MODE;
+    DB *db = new DB(mbdir.c_str(), options, 128LL*1024*1024, 128LL*1024*1024);
+    assert(db->is_open());
+    MBData mbd;
+
+    TestKey mkey(MABAIN_TEST_KEY_TYPE_SHA_256);
+    for(int i = 0; i < 10000; i++) {
+        kv = mkey.get_key(i);
+        assert(db->Remove(kv) == MBError::SUCCESS);
+    }
+
+    db->Close();
+    delete db;
+}
+
+
+static void GarbageCollectResources()
+{
+    std::cout << "\nKick-start Garbage Collection" << std::endl;
+    TestKey mkey(MABAIN_TEST_KEY_TYPE_SHA_256);
+    std::string kv;
+
+    int options = CONSTS::WriterOptions() | CONSTS::ReaderOptions() | CONSTS::ASYNC_WRITER_MODE;
+    DB *db = new DB(mbdir.c_str(), options, 128LL*1024*1024, 128LL*1024*1024);
+    assert(db->is_open());
+    MBData mbd;
+
+    db->CollectResource(1, 1);
+
+    for(int i = 0; i < max_key; i++) {
+        assert(db->Add(key_list[i], key_list[i]) == MBError::SUCCESS);
+    }
+
+    db->Close();
+    delete db;
+}
+
+
+// Multiple threads performing DB insertion/deletion/updating
+int main(int argc, char *argv[])
+{
+    std::string cmd = std::string("rm -rf ") + mbdir;
+    if(system(cmd.c_str()) != 0) {}
+
+    cmd = std::string("mkdir -p ") + mbdir;
+    if(system(cmd.c_str()) != 0) {}
+
+    pthread_t pid[256];
+    int nthread = 4;
+    if(nthread > 256) {
+        abort();
+    }
+
+    if(argc > 1) {
+        mbdir = std::string(argv[1]);
+        std::cout << "Mabain test db directory " << mbdir << "\n";
+    }
+    if(argc > 2) {
+        max_key = atoi(argv[2]);
+        std::cout << "Setting number of keys to be " << max_key << "\n";
+    }
+
+    SetTestStatus(false);
+    mabain::DB::SetLogFile(mbdir + "/mabain.log");
+
+    write_index.store(0, std::memory_order_release);
+    // Writer needs to enable async writer mode.
+    int options = CONSTS::WriterOptions() | CONSTS::ASYNC_WRITER_MODE;
+    DB *db = new DB(mbdir.c_str(), options, 128LL*1024*1024, 128LL*1024*1024);
+    assert(db->is_open());
+    db->RemoveAll();
+
+    for(int i = 0; i < nthread; i++) {
+        if(pthread_create(&pid[i], NULL, insert_thread, db) != 0) {
+            std::cout << "failed to create thread\n";
+            abort();
+        }
+    }
+
+    while(!stop_processing) {
+        usleep(5);
+    }
+
+    for(int i = 0; i < nthread; i++) {
+        pthread_join(pid[i], NULL);
+    }
+
+    // Writer handle must be the last one to close if reader handles are used for DB update.
+    assert(db->Close() == MBError::SUCCESS);
+    delete db;
+
+    // at this timer we added all required keys, verify all those keys
+    Lookup();
+
+    // delete keys so that we can create some garbage data to be reclaimed
+    Deletekeys();
+
+    // kick start the garbage collection task
+    GarbageCollectResources();
+
+    // lookup few keys that we added during garbage collection
+    GarbageLookup();
+
     mabain::DB::CloseLogFile();
+    SetTestStatus(true);
     return 0;
 }
